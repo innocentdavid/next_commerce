1555b6a7151601a187b999a3b8005b76ff4f23f8
innocentdavid <paulinnocent04@gmail.com>
2024-01-27 11:43:04 +0100

    s

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import LayoutError from '../layouts/LayoutError'
+import React from "react";
+import LayoutError from "../layouts/LayoutError";
+import Link from "next/link";
 
 const ErrorPage = () => (
   <LayoutError>
@@ -7,10 +8,12 @@ const ErrorPage = () => (
       <div className="container">
         <h1>Error 404</h1>
         <p>Woops. Looks like this page doesn't exist</p>
-        <a href="#" className="btn btn--rounded btn--yellow">Go to home</a>
+        <Link href="/" className="btn btn--rounded btn--yellow">
+          Go to home
+        </Link>
       </div>
     </section>
   </LayoutError>
-)
+);
 
-export default ErrorPage
+export default ErrorPage;
